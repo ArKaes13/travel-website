676835f2fdce5152f7540c52fc25288b90580563
Tyler <ArKaes1820@yahoo.com>
2023-08-10 22:23:25 -0700

    navbar mobile styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from './Navbar.js';
 
 function App() {
     return (
-        <h1>Hello</h1>
-    )
-}
+        <Navbar />
+    );
+};
 
 export default App;
