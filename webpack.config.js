8a8ce73a8071e2f84f835e393fb9015a0a668add
Tyler <ArKaes1820@yahoo.com>
2023-08-10 18:46:58 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    output: {
+        path: __dirname,
+        filename: 'index.bundle.js',
+    },
+    devServer: {
+        port: 3000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    miniCssExtractPlugin.loader,
+                    'css-loader',
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [new miniCssExtractPlugin()],
+};
